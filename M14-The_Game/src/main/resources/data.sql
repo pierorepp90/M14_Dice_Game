ea7d0457667df2036bea5b6dbf146e3b17d7f6f7
pierorepp90 <pierorepp90@gmail.com>
2020-11-25 13:03:55 +0100

    DEFAULT NULL
    All setted to default null in the script data.sql

--- M14-The_Game/src/main/resources/data.sql
+++ M14-The_Game/src/main/resources/data.sql
@@ -6,18 +6,18 @@ DROP TABLE IF EXISTS `players`;
 
 CREATE TABLE `players` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
-  `account_time` datetime NOT NULL,
-  `user_name` varchar(250) NOT NULL,
-  `password` varchar(250) NOT NULL,
+  `account_time` datetime DEFAULT NULL,
+  `user_name` varchar(250) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
   `succes_rate` double(19,4) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `dices` (
   `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `roll_time` datetime NOT NULL,
-  `dice_roll_1` int(2) NOT NULL,
-  `dice_roll_2` int(2) NOT NULL,
+  `roll_time` datetime DEFAULT NULL,
+  `dice_roll_1` int(2) DEFAULT NULL,
+  `dice_roll_2` int(2) DEFAULT NULL,
   `win` tinyint(1) DEFAULT NULL,
 
   `player_id` int(50) NOT NULL,
